e720275b1fa1d5e26d461652523b886635228142
mauroCh93 <mauro.rocha@fractalsoft.ec>
2025-08-06 15:17:12 -0500

    api dental

--- routes/api.php
+++ routes/api.php
@@ -10,7 +10,17 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-Route::apiResource('dental', DentalController::class);
+//Route::apiResource('dental', DentalController::class);
+
+// Rutas individuales para DentalController
+Route::get('dental-list', [DentalController::class, 'index']);           // Listar todos
+Route::post('dental', [DentalController::class, 'store']);          // Crear nuevo
+Route::get('dental/{dental}', [DentalController::class, 'show']);   // Mostrar uno
+Route::put('dental/{dental}', [DentalController::class, 'update']); // Actualizar uno
+Route::patch('dental/{dental}', [DentalController::class, 'update']); // Actualizar parcialmente
+Route::delete('dental/{dental}', [DentalController::class, 'destroy']); // Eliminar uno
+
+
  Route::middleware('auth')->group(function () {
         Route::get('/user', [AuthController::class, 'me']);
 
